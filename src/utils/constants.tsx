039233709290453ec1bbaa674656e5b3bc1af935
haskx <haskx@qq.com>
2025-04-29 08:09:42 +0800

    dsd

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,5 +1,5 @@
 
 export const constants = {
     "domain":"https://timercountdown.github.io",
-    "durations":[2,5,8,9,10,13,14,15,17,18,19,23,25,29,30,35]
+    "durations":[2,5,8,9,10,12,13,14,15,17,18,19,23,25,27,29,30,32,35,45,55,75,80,90]
 };
