d5a0223adbd8de288dec2f59721a8dc0b2124bf0
haskx <haskx@qq.com>
2025-05-01 19:35:08 +0800

    cc

--- src/app/days-until/page.tsx
+++ src/app/days-until/page.tsx
@@ -1,19 +1,15 @@
 import HolidayPageClient from "./page-client";
 
 
-interface PageProps {
-    params: {
-      slug: string;
-    };
-  }
 
-const HolidayPage: React.FC<PageProps> = async ({ params }) => {
+
+const HolidayPage: React.FC = async () => {
  
 
     return <HolidayPageClient />
 }
 
-export async function generateMetadata({ params }) {
+export async function generateMetadata() {
   return {
     title: `How many days until holiday`,
     description: `Countdown to holiday`,
