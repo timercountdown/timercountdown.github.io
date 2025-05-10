17356068301518c83a27f5c434917b6e5a68c806
haskx <haskx@qq.com>
2025-05-10 09:05:28 +0800

    cc

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,6 +1,6 @@
 
 export const constants = {
     "domain":"https://timercountdown.github.io",
-    "durations":[1,2,3,4,5,8,9,10,12,13,14,15,17,18,19,23,25,27,29,30,32,35,45,55,75,80,90,120],
+    "durations":[1,2,3,4,5,8,9,10,12,13,14,15,17,18,19,20,23,25,27,29,30,32,35,40,45,50,55,75,80,90,100,120],
     "timeUnits": ['second', 'minute', 'hour', 'day']
 };
